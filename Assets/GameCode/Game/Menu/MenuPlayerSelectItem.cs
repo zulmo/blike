a32214a6fe2b17cc8cf2e752da305b869ff6c2f0
Cyril Kamowski <cyril.kamowski@gmail.com>
2019-03-15 09:10:37 -0400

    Code cleaning

--- Assets/GameCode/Game/Menu/MenuPlayerSelectItem.cs
+++ Assets/GameCode/Game/Menu/MenuPlayerSelectItem.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class MenuPlayerSelectItem : MonoBehaviour
 {
+    private static string PlayerNameFormat = "Player {0}";
+
     [SerializeField]
     private Text _playerText;
 
@@ -16,7 +18,7 @@ public class MenuPlayerSelectItem : MonoBehaviour
 
     public void Open(int playerIndex, Color color)
     {
-        _playerText.text = string.Format("Player {0}", playerIndex + 1);
+        _playerText.text = string.Format(PlayerNameFormat, playerIndex + 1);
         _playerColor.gameObject.SetActive(true);
         _playerColor.material.color = color;
     }
